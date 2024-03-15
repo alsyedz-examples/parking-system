32b5b12c634062d46a1ebb3274ebac1991f4f7dd
syed <syednaeem15191@gmail.com>
2024-03-15 14:49:00 +0000

    parking spot test added

---
+++ api/tests/ParkingSpotsTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests;
+
+use Laravel\Lumen\Testing\DatabaseMigrations;
+
+class ParkingSpotsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @test
+     * @return void
+     */
+    public function test_that_index_endpoint_of_parking_spots_api_returns_a_successful_response()
+    {
+        $this->artisan('db:seed');
+
+        $response = $this->get('/api/v1/parking-spots', ['Accept' => 'application/json']);
+
+        $response->assertResponseOk();
+
+        $response->seeJsonStructure(['data']);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @test
+     * @return void
+     */
+    public function test_that_show_endpoint_of_parking_spots_api_returns_a_successful_response()
+    {
+        $this->artisan('db:seed');
+
+        $response = $this->get('/api/v1/parking-spots/1', ['Accept' => 'application/json']);
+
+        $response->assertResponseOk();
+
+        $response->seeJsonStructure(['id', 'label', 'price', 'booked_slots']);
+    }
+
+    /**
+     * A basic test example.
+     *
+     * @test
+     * @return void
+     */
+    public function test_that_show_endpoint_of_parking_spots_api_returns_a_unsuccessful_response()
+    {
+        $response = $this->get('/api/v1/parking-spots/1', ['Accept' => 'application/json']);
+
+        $response->assertResponseStatus(404);
+
+        $response->seeJsonStructure(['message']);
+    }
+}
